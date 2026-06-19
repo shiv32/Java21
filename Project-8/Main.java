6c0f12b746ccae32d9519a15632b49bc1d39b30a
shiv32 <yadav.shivkumar5@gmail.com>
2026-06-19 19:32:53 +0530

    sout

---
+++ Project-8/Main.java
@@ -0,0 +1,14 @@
+
+import java.io.PrintStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+      
+      System.out.println("hello");
+
+      PrintStream ps = System.out;
+      ps.println("hello2");
+       
+    }
+}
