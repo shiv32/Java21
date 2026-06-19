1120479ef9d900794f634f132e596ee455dbd1a7
shiv32 <yadav.shivkumar5@gmail.com>
2026-06-19 20:04:35 +0530

    test

--- Project-1/Main.java
+++ Project-1/Main.java
@@ -1,5 +1,32 @@
+
+class Test {
+
+    private String name = "Shiv Kumar";
+
+    public String getName() {
+        return name;
+    }
+}
+
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Hello from Java Console App");
+
+        var m_int = 1;
+        var m_char = "S";
+        var m_string = "shiv";
+
+        var test = new Test();
+        Test test2 = new Test();
+
+        System.out.println(m_int);
+        System.out.println(m_char);
+        System.out.println(m_string);
+        
+        System.out.println(test.getName());
+        System.out.println(test2.getName());
+
     }
 }
